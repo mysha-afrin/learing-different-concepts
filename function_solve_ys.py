b83c1820d0ffcaad3ca70d79d162ebd75f93cf13
myshaafrin <myshaafrinjeba916@gmail.com>
2026-09-14 10:33:34 +0600

    love you

---
+++ function_solve_ys.py
@@ -0,0 +1,20 @@
+#WAF to print the length of a list (list is the parameter)
+cities = ['Dhaka', 'Chittagong', 'Khulna', 'Rajshahi', 'Barisal', 'Sylhet', 'Pabna']
+
+def print_length(citiies):
+    print(len(cities))
+    return len(citiies)
+
+print_length(cities)
+
+
+
+
+
+
+
+#WAF to print the elements of a list in a single line (list is the parameter)
+def print_elements(cities):
+    print(cities)
+    return
+print_elements(cities)
